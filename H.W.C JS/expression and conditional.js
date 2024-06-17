e3bca090810d40935a004b5ec4750043c708a0cb
arena-abhishek <apdesign0001@gmail.com>
2024-06-17 18:43:00 +0530

    add newa changes

---
+++ H.W.C JS/expression and conditional.js
@@ -0,0 +1,5 @@
+//Use logical operator to find whether the age of a person lies betweein 10 and 20?
+//Demonstrate the use of switch case statements in Javascript.
+//Write a JavaScript program to find whether a number is Divisble by 2 and 3. 
+//Write a JavaScript program to find whether a number is Divisble by either 2 or 3.
+//Print "you can Drive "or"you cannaot Drive" based on age being greater than 18 using ternary opertator. 
