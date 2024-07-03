977844316f6f940bb527d9692c7aba20d73c2065
arena-abhishek <apdesign0001@gmail.com>
2024-07-03 12:45:20 +0530

    add

--- Calculator/cal.js
+++ Calculator/cal.js
@@ -1,9 +1,12 @@
-// let button = document.getElementsByClassName("button");
+let button = document.querySelector(".button");
+let funcbtn = document.querySelector(".funcbtn")
+let calbtn = document.querySelector(".calbtn")
 let input = document.getElementById("screen");
-// let mod = document.getElementById("mod");
-// let body = document.querySelector("body");
-// let moon = document.getElementById("btn").firstElementChild;
-// let sun = document.getElementById("btn").lastElementChild;
+let mod = document.getElementById("mod");
+let body = document.querySelector("body");
+let moon = document.getElementById("mod").firstElementChild;
+let sun = document.getElementById("mod").lastElementChild;
+
 
 function valuebtn(e) {
   input.value += e.value;
@@ -22,14 +25,31 @@ function calculate() {
   input.value = eval(input.value);
 }
 
-// mod.addEventListener("click", () => {
-//   if (body.style.background != "black") {
-//     body.style.background = "black";
-//     button.style.color = "white";
-//     moon.style.display = "none";
-//     sun.style.display = "block";
-//   }
-//   else{
-    
-//   }
-// });
+mod.addEventListener("click", () => {
+  if (body.style.background != "black") {
+    body.style.background = "black";
+    button.style.color = "white";
+    funcbtn.style.color = "white";
+    calbtn.style.color = "white";
+    input.style.color ="white"
+    input.style.backgroundColor ="black"
+    button.style.background = "#2f3035";
+    funcbtn.style.background = "#2f3035";
+    calbtn.style.background = "#2f3035";
+    moon.style.display = "none";
+    sun.style.display = "block";
+  }
+  else{
+    body.style.background = "#f1f0f7";
+    button.style.color = "black";
+    funcbtn.style.color = "white";
+    calbtn.style.color = "white";
+    input.style.color ="black"
+    input.style.backgroundColor ="#fdfbfe"
+    button.style.background = "#2f3035";
+    funcbtn.style.background = "#2f3035";
+    calbtn.style.background = "#2f3035";
+    moon.style.display = "block";
+    sun.style.display = "none";  
+  }
+});
