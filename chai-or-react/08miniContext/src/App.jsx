1066572453558363a3dbb9dc6a0233e680aca2f8
arena-abhishek <apdesign0001@gmail.com>
2025-02-18 21:30:53 +0530

    add

--- chai-or-react/08miniContext/src/App.jsx
+++ chai-or-react/08miniContext/src/App.jsx
@@ -1,12 +1,19 @@
-import UserContextProvider from "./context/UserContextProvider";
-import "./App.css";
+
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
-  return <UserContextProvider>
-    <h1>React with chai and share is important</h1>
-  </UserContextProvider>;
+  return (
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
 }
 
-export default App;
+export default App
--- chai-or-react/08miniContext/src/components/profile.jsx
+++ chai-or-react/08miniContext/src/components/profile.jsx
@@ -1,30 +1,12 @@
-import React, {useState, useContext} from 'react'
+import React, {useContext} from 'react'
 import UserContext from '../context/UserContext'
-function Pogin(){
 
-  const [ username, setUsername] = useState('')
-  const [ password, setPassword] = useState('')
+function Profile() {
+    const {user} = useContext(UserContext)
+    
+    if (!user) return <div>please login</div>
 
-  const {setUser} = useContext(UserContext)
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    setUser({username, password})
-  }
-
-  return (
-    <div>
-      <h2>Login</h2>
-      <input type="text"
-      value={username}
-      onChange={(e)=> setUsername(e.target.value)}
-       placeholder='username' />
-      <input type="text" 
-       value={password}
-       onChange={(e)=> setPassword(e.target.value)}
-      placeholder='password' />
-      <button onClick={handleSubmit}>Submit</button>
-    </div>
-  )
+    return <div>Welcome {user.username}</div>
 }
 
 export default Profile
