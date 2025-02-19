b5fee14ea3063fa4974a21743d6342da8836aa4c
arena-abhishek <apdesign0001@gmail.com>
2025-02-19 21:18:24 +0530

    add

---
+++ chai-or-react/reduxToolkitTodo/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Make sure this path is correct based on your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
