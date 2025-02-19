07a547b5ed20f4cc45ecdac9175f15ee350c9a41
arena-abhishek <apdesign0001@gmail.com>
2025-02-19 15:29:12 +0530

    add

---
+++ chai-or-react/09themeSwitcher/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Make sure this path is correct based on your file structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
